6ba7735e2b57aa0ef4d8dce0d7b257c709b7fac9
JacobTfar <103297328+JacobTfar@users.noreply.github.com>
2025-07-31 20:52:15 -0400

    Fix Netlify deployment by specifying correct build output directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://neonmint.efeele.dev",
+  outDir: '_site',
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
